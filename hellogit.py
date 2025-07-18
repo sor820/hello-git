d68f8bed2b44aa9bc2637993846e7318adcdd61b
sor820 <jonabb810@hotmail.com>
2025-07-18 17:39:13 -0600

    se añade lineas de prueba

--- hellogit.py
+++ hellogit.py
@@ -3,3 +3,8 @@ print("This is a new line in the file")
 print("This is a new line in the file")
 
 print("rama login prueba")
+
+print("This is a new line in the file")
+
+print("This is a new line in the file")
+print("This is a new line in the file")
